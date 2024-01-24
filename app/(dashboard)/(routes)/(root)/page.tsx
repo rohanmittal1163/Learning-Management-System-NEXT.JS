62f484bb0ccbeb8efab6752660e5880fdcf6c243
Rohan mittal <rohanmittal525@gmail.com>
2024-01-24 23:34:06 +0530

    feat:Responsive dashboard

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -36,7 +36,7 @@ export default async function Root() {
 					val={completedFields}
 				/>
 			</div>
-			<div className="grid grid-cols-4 gap-5">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-5 ">
 				{items.map(
 					(
 						item: Purchase & {
